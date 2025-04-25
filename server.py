8ac3d5e8937a72dcd7244a4d7a0b22239ad95eca
Yusuf Emre Bayrakcı <ybayrakci04@gmail.com>
2025-04-25 11:40:08 +0300

    added saving arduino data to arduino_data database

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional
 import mysql.connector
 from mysql.connector import Error
+
+import dbconfig # This should be the path to your dbconfig.py file
 
 
 app = FastAPI()
@@ -19,14 +21,49 @@ class SensorData(BaseModel):
     latitude: Optional[float] = None
     longitude: Optional[float] = None
 
+def insert_sensor_data(data: SensorData):
+    try:
+        # Establish connection
+        connection = mysql.connector.connect(**dbconfig.db_config)
+        cursor = connection.cursor()
+
+        # SQL query to insert data
+        insert_query = """
+        INSERT INTO arduino_data (
+            timestamp, temperature, humidity, pm25, pm10, co2, voc, latitude, longitude
+        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+
+        values = (
+            data.timestamp,
+            data.temperature,
+            data.humidity,
+            data.pm25,
+            data.pm10,
+            data.co2,
+            data.voc,
+            data.latitude,
+            data.longitude
+        )
+
+        cursor.execute(insert_query, values)
+        connection.commit()
+
+    except Error as e:
+        print(f"Error while inserting data: {e}")
+        raise HTTPException(status_code=500, detail="Database insertion failed")
+
+    finally:
+        if cursor:
+            cursor.close()
+        if connection.is_connected():
+            connection.close()
+
 @app.post("/arduino-data")
 async def receive_data(data: SensorData):
-    
    
-
-
-
-
+    insert_sensor_data(data)
+    
     response = {
         "success": True,
         "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
